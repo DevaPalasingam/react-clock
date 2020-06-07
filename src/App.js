7c2380478fc3b3f8d2cf7ef9422daf746ab7641b
Deva <Mac@deva-p.lan>
2020-06-07 14:40:26 -0500

    pushing to github

--- src/App.js
+++ src/App.js
@@ -4,23 +4,49 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Clock />
   );
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getTime();
+  }
+
+  componentDidMount() {
+    this.setTimer();
+  }
+
+  setTimer(){
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+  }
+
+  updateClock(){
+    this.setState(this.getTime, this.setTimer);
+  }
+
+  getTime() {
+    const currentTime = new Date();
+    return {
+      hours: currentTime.getHours(),
+      minutes: currentTime.getMinutes(),
+      seconds: currentTime.getSeconds(),
+      ampm: currentTime.getHours() >= 12 ? 'pm' : 'am'
+    }
+  }
+
+  render() {
+    const {hours, minutes, seconds, ampm} = this.state;
+      return (
+        <div className="clock">
+          {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
+          {minutes > 9 ? minutes : '0${minutes}'}:
+          {seconds > 9 ? seconds : '0${seconds}'} {ampm}
+        </div>
+      );
+  }
 }
 
 export default App;
