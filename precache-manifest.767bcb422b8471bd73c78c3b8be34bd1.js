b919af07cb956e61faabb4fc0903b669138d4857
Deva <Mac@deva-p.lan>
2020-06-07 13:28:46 -0500

    Updates

---
+++ precache-manifest.767bcb422b8471bd73c78c3b8be34bd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cc69f52f0b6e8435466efa75e6d76f",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "1705b0f70da8a77fafc5",
+    "url": "/react-clock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f31b9eb160fb0a96e52",
+    "url": "/react-clock/static/js/2.ab5035ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clock/static/js/2.ab5035ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1705b0f70da8a77fafc5",
+    "url": "/react-clock/static/js/main.cea34064.chunk.js"
+  },
+  {
+    "revision": "426f766e591a02ba8368",
+    "url": "/react-clock/static/js/runtime-main.165a8549.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
